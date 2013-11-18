38722733a39ab7c8bd2ada65bcdae1af4f47dae7
Raffi Bedikian <rbedikian@leapmotion.com>
2013-11-17 19:39:49 -0800

    Increase menu opacity a bit

--- clay/include/UserInterface.h
+++ clay/include/UserInterface.h
@@ -341,7 +341,7 @@ public:
 private:
 
   static float getOpacity(float activation) {
-    static const float NORMAL_OPACITY = 0.4f;
+    static const float NORMAL_OPACITY = 0.6f;
     static const float ACTIVATED_OPACITY = 1.0f;
     return g_sculptMult.value * (NORMAL_OPACITY + (ACTIVATED_OPACITY-NORMAL_OPACITY)*activation);
   }
