e56a5a357586977eddbf3cdaf080a1c33166887c
Adrian Gasinski <agasinski@leapmotion.com>
2013-11-19 22:36:19 -0800

    Create ReplayUtil.

---
+++ clay/include/ReplayUtil.h
@@ -0,0 +1,217 @@
+#pragma once
+#ifndef __ReplayUtil_h__
+#define __ReplayUtil_h__
+
+#include <vector>
+#include <streambuf>
+#include <iostream>
+
+#define LM_USE_ITEMS 0
+
+#ifndef LM_BREAK
+# define LM_BREAK __asm { int 3 }
+#endif
+#ifndef LM_ASSERT
+# define LM_ASSERT(condition, message) if (!(condition)) LM_BREAK;
+#endif
+
+
+class ReplayUtil
+{
+public:
+  static ReplayUtil& getInstance() { static ReplayUtil instance; return instance; }
+
+  enum Mode {
+    MODE_INACTIVE = -1,
+    MODE_RECORD,
+    MODE_REPLAY
+  };
+
+  struct Item {
+    union {
+      float m_a;
+      double m_b;
+      int m_c;
+      char m_buffer[32];
+    } m_value;
+
+    float asFloat() { return m_value.m_a; }
+    double asDouble() { return m_value.m_b; }
+
+    friend std::ostream& operator << (std::ostream& os, const Item& i);
+    friend std::istream& operator >> (std::istream& is, Item& i);
+  };
+
+  ReplayUtil();
+  ~ReplayUtil();
+
+  void setMode(Mode mode);
+  Mode getMode() const { return m_mode; }
+
+  void loadHistory();
+  void clearHistoryFile();
+  void saveHistory();
+
+  void addItem(const Item& item);
+  const Item& getItem();
+
+  void trackItem(Item& item);
+
+  template<typename T> void addObject( const T& obj );
+  template<typename T> void getObject(T* obj);
+  template<typename T> void trackObject(T& obj);
+
+  template <typename T> void trackValue(T& value);
+  template <typename T> void trackConstValue(const T& item);
+
+  template <typename T> T returnTrackedValue(const T& value);
+  template <typename T> T returnTrackedValue();
+
+  template <typename T>
+  void assertIdentical(const T& value);
+
+private:
+  void initMode();
+
+  Mode m_mode;
+
+  std::vector<Item> m_history;
+  int m_historyIdx;
+
+  std::vector<char> m_historyStream;
+  int m_historyStreamIdx;
+
+  static const char* configFilename;
+  static const char* historyFilename;
+};
+
+std::ostream& operator << (std::ostream& os, const ReplayUtil::Item& i);
+std::istream& operator >> (std::istream& is, ReplayUtil::Item& i);
+
+#define LM_START_RECORD() ReplayUtil::getInstance().setMode(ReplayUtil::MODE_RECORD)
+#define LM_START_REPLAY() ReplayUtil::getInstance().setMode(ReplayUtil::MODE_REPLAY)
+
+#define LM_TRACK_IS_RECORDING() (ReplayUtil::getInstance().getMode() == ReplayUtil::MODE_RECORD)
+#define LM_TRACK_IS_REPLAYING() (ReplayUtil::getInstance().getMode() == ReplayUtil::MODE_REPLAY)
+
+#define LM_TRACK_VALUE(value) ReplayUtil::getInstance().trackValue(value)
+#define LM_TRACK_CONST_VALUE(value) ReplayUtil::getInstance().trackConstValue(value)
+
+#define LM_RETURN_TRACKED(value) ReplayUtil::getInstance().returnTrackedValue(value)
+#define LM_RETURN_TRACKED_NEXT(type) ReplayUtil::getInstance().returnTrackedValue<type>()
+
+#define LM_RETURN_TRACKED_CONDITIONAL(condition, value, type) (condition?LM_RETURN_TRACKED(value):LM_RETURN_TRACKED_NEXT(type))
+
+#define LM_ASSERT_IDENTICAL(value) ReplayUtil::getInstance().assertIdentical(value)
+
+#define LM_TRACK_TODO() 
+
+template <bool b> struct StaticAssert {};
+template <> struct StaticAssert<true> { static void Assert() {} };
+#define LM_COMPILE_TIME_ASSERT(condition) StaticAssert<condition>::Assert();
+
+//
+// Base object storing
+//
+
+template<typename T>
+void ReplayUtil::addObject( const T& obj )
+{
+  int streamEndIdx = m_historyStream.size();
+  m_historyStream.resize(streamEndIdx + sizeof(obj));
+  T* result = reinterpret_cast<T*>(&m_historyStream[0]+streamEndIdx);
+  std::memcpy(result, &obj, sizeof(obj));
+  //*result = item;
+}
+
+template<typename T>
+void ReplayUtil::getObject(T* obj)
+{
+  // assert in bounds
+  const T* result = reinterpret_cast<const T*>(&m_historyStream[0]+m_historyStreamIdx);
+  m_historyStreamIdx += sizeof(T);
+  std::memcpy(obj, result, sizeof(T));
+  //std::memcpy(const_cast<void*>(reinterpret_cast<const void*>(obj)), result, sizeof(T));
+}
+
+template<typename T>
+void ReplayUtil::trackObject(T& obj) {
+  if (m_mode == MODE_RECORD) {
+    addObject(obj);
+    saveHistory();
+  } else if (m_mode == MODE_REPLAY) {
+    getObject(&obj);
+  }
+}
+
+
+//
+// User functions
+//
+
+template <typename T>
+void ReplayUtil::trackValue(T& value) {
+#if LM_USE_ITEMS
+  Item container;
+  LM_COMPILE_TIME_ASSERT(sizeof(Item) >= sizeof(T));
+  *reinterpret_cast<T*>(&container) = value;
+  trackItem(container);
+  value = *reinterpret_cast<T*>(&container);
+#else
+  trackObject(value);
+#endif
+}
+
+template <typename T>
+void ReplayUtil::trackConstValue(const T& value) {
+#if LM_USE_ITEMS
+  Item container;
+  LM_COMPILE_TIME_ASSERT(sizeof(Item) >= sizeof(T));
+  *reinterpret_cast<T*>(&container) = value;
+  trackItem(container);
+  const_cast<T&>(value) = *reinterpret_cast<T*>(&container);
+#else
+  trackObject(const_cast<T&>(value));
+#endif
+}
+
+template <typename T>
+T ReplayUtil::returnTrackedValue(const T& value) {
+#if LM_USE_ITEMS
+  Item container;
+  LM_COMPILE_TIME_ASSERT(sizeof(Item) >= sizeof(T));
+  *reinterpret_cast<T*>(&container) = value;
+  trackItem(container);
+  T result  = *reinterpret_cast<T*>(&container);
+  return result;
+#else
+  T result = value;
+  trackObject(result);
+  return result;
+#endif
+}
+
+template <typename T>
+T ReplayUtil::returnTrackedValue() {
+  LM_ASSERT(m_mode == MODE_REPLAY, "Trying to extract recorded value (condition not met) during recording.")
+#if LM_USE_ITEMS
+  trackItem(container);
+  T result  = *reinterpret_cast<T*>(&container);
+  return result;
+#else
+  T result;
+  trackObject(result);
+  return result;
+#endif
+}
+
+template <typename T>
+void ReplayUtil::assertIdentical(const T& value) {
+  T copy = value;
+  T recorded = returnTrackedValue(value);
+  LM_ASSERT(m_mode != MODE_REPLAY || copy == recorded, "LM_ASSERT_IDENTICAL failed.");
+}
+
+
+
+#endif // __ReplayUtil_h__
---
+++ clay/src/ReplayUtil.cpp
@@ -0,0 +1,179 @@
+#include "StdAfx.h"
+#include "ReplayUtil.h"
+
+#include <iostream>
+#include <fstream>
+
+//#include <boost/filesystem.hpp>
+
+const char* ReplayUtil::configFilename = "ReplayUtil.cfg";
+const char* ReplayUtil::historyFilename = "ReplayHistory.dat";
+
+ReplayUtil::ReplayUtil()
+{
+  initMode();
+}
+
+ReplayUtil::~ReplayUtil()
+{
+
+}
+
+void ReplayUtil::initMode()
+{
+  std::ifstream file(configFilename);
+
+  // Does config file exist
+  if (file.good()) {
+    // Determine mode
+    char buffer[1024];
+    file >> buffer;
+    if (0 == std::strcmp(buffer, "RECORD")) {
+      setMode(MODE_RECORD);
+    } else if (0 ==std::strcmp(buffer, "REPLAY")) {
+      setMode(MODE_REPLAY);
+    } else {
+      setMode(MODE_INACTIVE);
+    }
+    file.close();
+  } else {
+    // Create the file
+    std::ofstream file(configFilename);
+    file << "RECORD" << std::endl;
+    file.close();
+    setMode(MODE_RECORD);
+  }
+}
+
+void ReplayUtil::setMode(Mode mode) {
+  m_mode = mode;
+  m_history.clear();
+  m_historyIdx = 0;
+  m_historyStream.clear();
+  m_historyStreamIdx = 0;
+
+  //delete m_historyIStream; 
+  //delete m_historyOStream;
+  //m_historyIStream = NULL;
+  //m_historyOStream = NULL;
+  //m_historyStreamBuf.
+
+  switch(m_mode) {
+  case MODE_RECORD:
+    clearHistoryFile();
+    break;
+  case MODE_REPLAY:
+    loadHistory();
+    break;
+  case MODE_INACTIVE:
+    // off
+    break;
+  }
+}
+
+void ReplayUtil::loadHistory()
+{
+  std::ifstream file(historyFilename, std::ios::binary);
+#if LM_USE_ITEMS
+  Item item;
+  file >> item;
+  while (file.good()) {
+    // Create item & push
+    m_history.push_back(item);
+    file >> item;
+  }
+#else
+  // Use stream
+  file.seekg(0, std::ios::end);
+  int size = (int)file.tellg();
+  m_historyStream.resize(size);
+  file.seekg(0, std::ios::beg);
+  if (m_historyStream.size())
+  {
+    file.read(&m_historyStream[0], size);
+  }
+  file.close();
+#endif
+}
+
+void ReplayUtil::clearHistoryFile() {
+  std::ofstream file(historyFilename, std::ios::binary | std::ios::trunc);
+  if (!file) {
+    std::cerr << "could not truncate" << std::endl;
+  }
+  file.flush();
+  file.close();
+}
+
+void ReplayUtil::saveHistory()
+{
+  std::ofstream file(historyFilename, std::ios::binary | std::ios::app);
+#if LM_USE_ITEMS
+  for (unsigned i = 0; i < m_history.size(); i++)
+  {
+    file << m_history[i];
+  }
+  m_history.clear();
+#else
+  if (m_historyStream.size())
+  {
+    file.write(&m_historyStream[0], m_historyStream.size());
+  }
+  m_historyStream.clear();
+#endif
+
+}
+
+void ReplayUtil::addItem( const Item& item )
+{
+  m_history.push_back(item);
+}
+
+const ReplayUtil::Item& ReplayUtil::getItem()
+{
+  // assert in bounds
+  return m_history[m_historyIdx++];
+}
+
+void ReplayUtil::trackItem(Item& item) {
+  if (m_mode == MODE_RECORD) {
+    addItem(item);
+    saveHistory();
+  } else if (m_mode == MODE_REPLAY) {
+    item = getItem();
+  }
+}
+
+
+//template <typename T>
+//void ReplayUtil::trackValue(T& value) {
+//  Item container;
+//  *reinterpret_cast<T*>(&container) = value;
+//  trackItem(container);
+//  value = *reinterpret_cast<T*>(&container);
+//}
+//
+//template <typename T>
+//void ReplayUtil::trackConstValue(const T& value) {
+//  Item container;
+//  *reinterpret_cast<T*>(&container) = value;
+//  trackItem(container);
+//  const_cast<T&>(value) = *reinterpret_cast<T*>(&container);
+//}
+
+//template void ReplayUtil::trackValue(float& item);
+//template void ReplayUtil::trackValue(int& item);
+//
+//template void ReplayUtil::trackConstValue(const float& item);
+//template void ReplayUtil::trackConstValue(const int& item);
+
+
+std::ostream& operator << (std::ostream& os, const ReplayUtil::Item& i) {
+  os.write(reinterpret_cast<const char*>(&i), sizeof(i));
+  return os;
+}
+
+std::istream& operator >> (std::istream& is, ReplayUtil::Item& i) {
+  is.read(reinterpret_cast<char*>(&i), sizeof(i));
+  return is;
+}
